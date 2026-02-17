c9257f67ace33046bb0ca42ddda5f42fe3d9f924
noahmudrick <noahmudrick@gmail.com>
2026-02-17 18:42:15 -0500

    Moved some files around
    Placed the zombies in the zombie file.

--- interfaces/functionality/IDrawable.cs
+++ interfaces/functionality/IDrawable.cs
@@ -1,3 +1,7 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
 public interface IDrawable
 {
     // The order of layer. For example, plants should be above lawn.
--- obj/Debug/net9.0/zombieBranch.AssemblyInfo.cs
+++ obj/Debug/net9.0/zombieBranch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zombieBranch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc083e1b3d9dbe9fa2ee44ac5de4ec04eeb4db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("zombieBranch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zombieBranch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
