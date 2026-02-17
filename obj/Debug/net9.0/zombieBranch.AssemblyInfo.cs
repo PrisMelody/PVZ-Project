4a1553918bfdfc5b98d18b26c9bfee32d6274c26
noahmudrick <noahmudrick@gmail.com>
2026-02-17 18:47:31 -0500

    Moved files around
    Moved more files around.

--- obj/Debug/net9.0/zombieBranch.AssemblyInfo.cs
+++ obj/Debug/net9.0/zombieBranch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zombieBranch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc083e1b3d9dbe9fa2ee44ac5de4ec04eeb4db3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9257f67ace33046bb0ca42ddda5f42fe3d9f924")]
 [assembly: System.Reflection.AssemblyProductAttribute("zombieBranch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zombieBranch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
