ffcbcaf2a9d23823b2ccde7b68c2255fb8983fa1
noahmudrick <noahmudrick@gmail.com>
2026-02-17 19:54:55 -0500

    Added some ":using"s to interfaces.

--- interfaces/functionality/IClickable.cs
+++ interfaces/functionality/IClickable.cs
@@ -1,4 +1,7 @@
 using System.Drawing;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 public interface IClickable
 {
--- interfaces/functionality/IUpdatable.cs
+++ interfaces/functionality/IUpdatable.cs
@@ -1,3 +1,6 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 public interface IUpdatable
 {
     void Update (Gametime gametime);
