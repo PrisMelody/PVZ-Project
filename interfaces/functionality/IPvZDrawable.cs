c2440b044d57fdfed4d898d9866c0b0cf2629ff0
Feder0419 <chen.13968@osu.edu>
2026-02-21 10:37:16 -0500

    rename interface to solve alias issues with MonoGame interface.

--- interfaces/functionality/IPvZDrawable.cs
+++ interfaces/functionality/IPvZDrawable.cs
@@ -2,7 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-public interface IDrawable
+public interface IPvZDrawable
 {
     // The order of layer. For example, plants should be above lawn.
     int DrawOrder {get;}
--- interfaces/functionality/IPvZUpdatable.cs
+++ interfaces/functionality/IPvZUpdatable.cs
@@ -1,7 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-public interface IUpdatable
+public interface IPvZUpdatable
 {
     void Update (GameTime gametime);
 }
