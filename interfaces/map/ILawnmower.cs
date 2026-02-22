2567f4382db34113d24cc45494d77106d4a57b05
ctcope228 <ctcope228@gmail.com>
2026-02-22 07:06:06 -0500

    test

--- interfaces/map/ILawnmower.cs
+++ interfaces/map/ILawnmower.cs
@@ -3,14 +3,9 @@ using Microsoft.Xna.Framework;
 public interface ILawnmower : IPvZDrawable, IPvZUpdatable, IDamageable
 {
     Vector2 Position { get; set; }
-
     int Row { get; }
-
     bool IsActivated { get; }
-
     bool IsMoving { get; }
-
     float Speed { get; }
-
     void Activate();
 }
